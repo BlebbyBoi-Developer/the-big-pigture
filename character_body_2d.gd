d93e78ce8e6de87edcf7fd305cc489b44579f409
BlebbyBoi-Developer <noahmauri34@gmail.com>
2026-05-01 13:28:06 +0800

    Applied animations and removed projectiles for bug fixing

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,25 +1,35 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		
+		# Flip sprite depending on direction
+		sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	# --- ANIMATIONS ---
+	if not is_on_floor():
+		sprite.play("Jump")
+	elif direction != 0:
+		sprite.play("Run")
+	else:
+		sprite.play("Idle")
